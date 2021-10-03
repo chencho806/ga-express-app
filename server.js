9504ea1f9e6615ebd6f6f2e707baa895442735f7
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-10-03 11:49:37 -0500

    updating code for heroku

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const MONGODB_URI = process.env.MONGODB_URI
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
+
 // mongoose.connect(MONGODB_URI, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
